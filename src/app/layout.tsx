6ebaca268e5b68683f797d20e7bca03c465ad4a8
Moi1oM <hcloud0806@gmail.com>
2025-09-29 00:09:37 +0900

    feat : vercel-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -114,6 +115,7 @@ export default function RootLayout({
             <CooldownTimer />
 
             {children}
+            <Analytics />
             <Toaster />
           </TRPCProvider>
         </body>
